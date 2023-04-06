f6536979985ff0ceab6c98a7e6c2d869dde2d637
Guilherme Borges <guilhermepardi@gmail.com>
2023-04-06 15:58:18 -0300

    Subindo a lógica de que, se tiver tasks, irá aparecer a lista, se não tiver irá aparecer a imgam

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,22 +1,27 @@
 import { AlertText, ClipboardImg, NoTasksContainer, WithTasks } from "./styles";
 import clipboardImg from "../../assets/clipboard.svg"
+import { useState } from "react";
 
 export function Home() {
+  const [tasks, setTasks] = useState([''])
+
   return (
     <>
-      <NoTasksContainer>
-        <ClipboardImg src={clipboardImg} alt="" />
+      { tasks.length > 1 ? (
+        <WithTasks>
+          <h1>Tasks</h1>
+        </WithTasks>      
+      ) : (
+        <NoTasksContainer>
+          <ClipboardImg src={clipboardImg} alt="" />
 
-        <AlertText>
-          <strong>Você ainda não tem tarefas cadastradas.</strong>
-          <br />
-          Crie tarefas e organize seus itens a fazer
-        </AlertText>
-      </NoTasksContainer>
-
-      <WithTasks>
-        <h1>Tasks</h1>
-      </WithTasks>
+          <AlertText>
+            <strong>Você ainda não tem tarefas cadastradas.</strong>
+            <br />
+            Crie tarefas e organize seus itens a fazer
+          </AlertText>
+        </NoTasksContainer>
+      )}
     </>
   )
 }
