56cc8d526206f9ee1cbb004381916bd672c07895
Guilherme Borges <guilhermepardi@gmail.com>
2023-04-06 09:03:25 -0300

    Subindo a criação do tema default

---
+++ src/styles/themes/defaultTheme.ts
@@ -0,0 +1,19 @@
+export const defaultTheme = {
+  white: '#ffffff',
+
+  'purple': '#8284fa',
+  'purple-dark': '#5e60ce',
+  'blue': '#4ea8de',
+  'blue-dark': '#1e6f9f',
+
+  'gray-700': '#0d0d0d',
+  'gray-600': '#1a1a1a',
+  'gray-500': '#262626',
+  'gray-400': '#333333',
+
+  'gray-300': '#808080',
+  'gray-200': '#d9d9d9',
+  'gray-100': '#f2f2f2',
+
+  'red': '#e25858'
+}
