89737800bf890d99efb0ab22055f0af40f5f30c9
Guilherme Borges <guilhermepardi@gmail.com>
2023-04-10 08:17:59 -0300

    Subindo a interface de TaskBar

--- src/pages/Home/TaskBar/index.tsx
+++ src/pages/Home/TaskBar/index.tsx
@@ -1,10 +1,15 @@
 import { Trash } from "phosphor-react";
+import { useState } from "react";
 import { TaskBarContainer } from "./styles";
 
-export function TaskBar({ task }: any) {
+interface TaskBarProps {
+  task: string
+}
+
+export function TaskBar({ task }: TaskBarProps) {
   return (
     <TaskBarContainer>
-      <input type="checkbox"/>
+      <input type="checkbox" />
       <p>{task}</p>
       <button>
         <Trash />
